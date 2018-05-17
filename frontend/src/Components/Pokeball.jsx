fcfa871d0cf32edc2ee679c5b21bbf080afc8a3e
ArlenZuno <arlenzuno@gmail.com>
2018-05-17 13:58:43 -0700

    working on searchbar

---
+++ frontend/src/Components/Pokeball.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// import { Route, Link } from "react-router-dom";
+
+class Pokeball extends Component {
+    render() {
+        return (
+            <div>
+            
+            </div>
+        )
+    }
+}
+
+export default Pokeball;
